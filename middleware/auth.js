c57ee1fc9d7cfd02624a0ba45ed10f138bd8e33e
Nadia Sokolova <nadyafane12@gmail.com>
2020-10-02 20:01:00 +0400

    React start

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,8 +12,8 @@ module.exports = (req,res,next) => {
 
  try {
   const decoded = jwt.verify( token, config.get('jwtSecret') )
-  req.user = decoded.user // we pass this very request to the req in the next auth file??
-  next() // WHAT IS HAPPENING IN THESE TWO
+  req.user = decoded.user // we pass this very request to the req in the next auth file?? --a- yes, and we append here
+  next() // WHAT IS HAPPENING IN THESE TWO --a- you know now
  } catch (err) {
   res.status(401).json({ msg: 'Token is not valid' })
  }
